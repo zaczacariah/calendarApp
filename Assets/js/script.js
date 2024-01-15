9a68f502f3ec55cfaa3c79b80aa378e242b0e7ff
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-01-15 18:55:24 +1030

    Adding comments to code that needed it and creating readme documentation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,7 @@
 
 $(function () {
+
+// Var Declarations
 var main = $('#main_container');
 var currentDay = $('#currentDay');
 var time = new Date($.now());
@@ -11,29 +13,32 @@ var options = {
   day: 'numeric',       
        
 };
-
 var formattedDate = time.toLocaleDateString('en-US', options);
-
-currentDay.append(formattedDate);
+currentDay.append(formattedDate); // Set Date
 
 
 time = time.getHours();
 
 // Create the internal items of each hour
+//
 function timeDivInside(item, timeFormatted){
+
   var internalDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3');
   internalDiv.html(item + timeFormatted);
   var textArea = $('<textarea>');
   textArea.addClass('col-8 col-md-10 description');
   textArea.attr('rows', '3');
 
-  //Get Existing Record
+  //Get Saved Records
   var savedLine = getStoredLine(`${item}-${timeFormatted}`);
   textArea.html(savedLine != false ? savedLine : '');
 
   var button = $('<button>').addClass('btn saveBtn col-2 col-md-1');
   button.attr('aria-label', 'save');
-  button.on('click', { 'textArea': textArea, 'id': `${item}-${timeFormatted}` }, saveEvent);
+
+  //Button Click Listener
+  //Passing textArea and id so I don't need to traverse Dom as much
+  button.on('click', { 'textArea': textArea, 'id': `${item}-${timeFormatted}` }, saveEvent); 
 
 
   var i = $('<i>').addClass('fas fa-save');
@@ -46,21 +51,23 @@ function timeDivInside(item, timeFormatted){
 
 
 
-// Setup up Calendar
+// Setup up Calendar 
 function InitCalendar(){
   [9, 10, 11, 12, 1, 2, 3, 4, 5].forEach((item, index) => {
 
-
     var div = $('<div>');
     div.addClass('row time-blow ' + getTimeClass(item, time));
+
     //is it Am or Pm?
     var timeFormatted = `${item < 13 && item > 8 ? 'am' : 'pm'}`;
     div.attr('id', `${item}-${timeFormatted}`);
-    // Get internal contents
-    var { internalDiv, textArea, button} = timeDivInside(item, timeFormatted); 
+
+    // Create internal contents
+    var { internalDiv, textArea, button } = timeDivInside(item, timeFormatted); 
+
+    // Append Items
     div.append(internalDiv);
     div.append(textArea);
-    
     div.append(button);
     main.append(div);
 
@@ -68,12 +75,14 @@ function InitCalendar(){
 }
 InitCalendar();
 
+// Get Past, Present or Future Class
+//
 function getTimeClass(item, currentTime) {
 
-    // Convert item to a 24-hour format
-    if (item < 9) {
-      item += 12; // Convert AM hours to PM (e.g., 9 AM becomes 9 + 12 = 21)
-    }
+  // Convert item to a 24-hour format
+  if (item < 9) {
+    item += 12; // Convert AM hours to PM (e.g., 9 AM becomes 9 + 12 = 21)
+  }
 
   if (item === currentTime) {
       return 'present';
@@ -84,31 +93,33 @@ function getTimeClass(item, currentTime) {
   }
 }
 
-
-function saveEvent({ target, data }){
+// Save Button Event
+//
+function saveEvent({ data }){
   var id = data.id;
   var value = data.textArea.val();
 
- 
-
   var currentStore = localStorage.getItem('schedule') || [];
 
+  //if currentStore not empty
   if(currentStore != ''){
     currentStore = JSON.parse(currentStore);
 
-    // returns false boolean or relevant index of record if exists
+    // Get index of current item or -1 if doesn't exist
     var index = currentStore.findIndex(function(item) {
       return item.id === id;
     });
-   
-      if(index != -1){
-        currentStore[index].value = value;
-      } else {
-        var toBeAdded = { 'id': id, 'value': value };
-        currentStore.push(toBeAdded);
-      }
-      localStorage.setItem("schedule", JSON.stringify(currentStore));
+    
+    // Schedule item exists and will be updated to new value else adds new
+    if(index != -1){
+      currentStore[index].value = value;
+    } else {
+      var toBeAdded = { 'id': id, 'value': value };
+      currentStore.push(toBeAdded);
+    }
+    localStorage.setItem("schedule", JSON.stringify(currentStore));
 
+  // If empty
   } else {
     var toBeAdded = { 'id': id, 'value': value };
     localStorage.setItem('schedule', JSON.stringify([toBeAdded]));
@@ -118,16 +129,19 @@ function saveEvent({ target, data }){
 }
 
 
-
+// Retrieve the stored value for scheduled our or if returns
+// false if doesn't exist
 function getStoredLine(id) {
   var currentStore = localStorage.getItem('schedule') || false;
 
+  // Break if currentStore doesn't exist
   if(currentStore === false){
     return false;
   } 
 
   currentStore = JSON.parse(currentStore);
 
+  // Retrieve index or -1
   var index = currentStore.findIndex(function(item) {
     return item.id === id;
   });
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# calendarApp
+#Calendar App
+
+## Description
+
+Calendar App is designed to serve as a scheduler for work between the hours of 9am to close at 5pm. Here you can enter your tasks for each hour of the day and it will be stored even if you refresh the page. 
+
+In this version, I have dynamically generated the containers for each hour and added event listeners to track clicks on save buttons where the content of that hour is saved in local storage. 
+
+Live Link:
+https://zaczacariah.github.io/calendarApp/
+
+## Screenshot
+![alt text](./assets/images/screenshot.png)
+
+## Installation
+
+N/A
+
+## Usage
+
+For use by anyone seeking a task tracker for their work schedule.
+
+## Credits
+ N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+ 
